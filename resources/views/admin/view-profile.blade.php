fbb42972f8289d794181c8f188d8f4547ea4390d
pentasoft <pentasoftrepo@gmail.com>
2024-12-26 17:59:34 +0530

    add

--- resources/views/admin/view-profile.blade.php
+++ resources/views/admin/view-profile.blade.php
@@ -2478,21 +2478,70 @@
             }
         </script>
         <script>
-            function calculateDays() {
-                const fromDate = document.querySelector('input[name="from"]').value;
-                const toDate = document.querySelector('input[name="to"]').value;
-                const noOfDaysInput = document.querySelector('input[name="no_of_days"]');
+             function calculateDays() {
+                    const fromDate = document.querySelector('input[name="from"]').value;
+                    const toDate = document.querySelector('input[name="to"]').value;
+                    const noOfDaysInput = document.querySelector('input[name="no_of_days"]');
 
-                if (fromDate && toDate) {
-                    const from = new Date(fromDate);
-                    const to = new Date(toDate);
-                    const timeDifference = to - from;
-                    const daysDifference = timeDifference / (1000 * 3600 * 24);
+                    if (fromDate && toDate) {
 
-                    noOfDaysInput.value = daysDifference >= 0 ? daysDifference : 0;
-                } else {
-                    noOfDaysInput.value = '';
-                }
+                        var startDate = new Date(fromDate);
+                        var endDate = new Date(toDate);
+
+                        const baseUrl = "{{ url('/admin') }}";
+                        fetch(baseUrl + '/get-holidays')
+                            .then(response => response.json())
+
+                            .then(holidays => {
+
+                                var holidayDates = holidays.map(holiday => new Date(holiday.holiday_date).toDateString());
+                                var diffDays = 0;
+                                for (var date = new Date(startDate); date <= endDate; date.setDate(date.getDate() + 1)) {
+
+                                    if (date.getDay() !== 0 && date.getDay() !== 6 && !holidayDates.includes(date
+                                            .toDateString())) {
+                                        diffDays++;
+                                    }
+                                }
+
+                                noOfDaysInput.value = diffDays;
+                            })
+                            .catch(error => console.error('Error fetching holidays:', error));
+                        }
+            }
+
+
+
+            function calculateDays1() {
+                    const fromDate = document.getElementById('from1').value;
+                    const toDate = document.getElementById('to1').value;
+                    const noOfDaysInput = document.getElementById('no_of_days1');
+
+                    if (fromDate && toDate) {
+
+                        var startDate = new Date(fromDate);
+                        var endDate = new Date(toDate);
+
+                        const baseUrl = "{{ url('/admin') }}";
+                        fetch(baseUrl + '/get-holidays')
+                            .then(response => response.json())
+
+                            .then(holidays => {
+
+                                var holidayDates = holidays.map(holiday => new Date(holiday.holiday_date).toDateString());
+                                var diffDays = 0;
+                                for (var date = new Date(startDate); date <= endDate; date.setDate(date.getDate() + 1)) {
+
+                                    if (date.getDay() !== 0 && date.getDay() !== 6 && !holidayDates.includes(date
+                                            .toDateString())) {
+                                        diffDays++;
+                                    }
+                                }
+
+                                noOfDaysInput.value = diffDays;
+                            })
+                            .catch(error => console.error('Error fetching holidays:', error));
+                        }
             }
         </script>
 
@@ -2576,22 +2625,22 @@
             });
         </script>
         <script>
-            function calculateDays() {
-                const fromDate = document.querySelector('input[name="from"]').value;
-                const toDate = document.querySelector('input[name="to"]').value;
-                const noOfDaysInput = document.querySelector('input[name="no_of_days"]');
+            // function calculateDays() {
+            //     const fromDate = document.querySelector('input[name="from"]').value;
+            //     const toDate = document.querySelector('input[name="to"]').value;
+            //     const noOfDaysInput = document.querySelector('input[name="no_of_days"]');
 
-                if (fromDate && toDate) {
-                    const from = new Date(fromDate);
-                    const to = new Date(toDate);
-                    const timeDifference = to - from;
-                    const daysDifference = timeDifference / (1000 * 3600 * 24);
+            //     if (fromDate && toDate) {
+            //         const from = new Date(fromDate);
+            //         const to = new Date(toDate);
+            //         const timeDifference = to - from;
+            //         const daysDifference = timeDifference / (1000 * 3600 * 24);
 
-                    noOfDaysInput.value = daysDifference >= 0 ? daysDifference + 1 : 0;
-                } else {
-                    noOfDaysInput.value = '';
-                }
-            }
+            //         noOfDaysInput.value = daysDifference >= 0 ? daysDifference + 1 : 0;
+            //     } else {
+            //         noOfDaysInput.value = '';
+            //     }
+            // }
         </script>
         <script>
             document.addEventListener("DOMContentLoaded", function() {
