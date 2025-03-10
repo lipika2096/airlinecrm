2f91870e0b74ccec70ed4e5f5452f1fa5cfd01a3
Lipika <dev.pentasoft@gmail.com>
2025-03-10 12:22:01 +0530

    added datatables to superadmin pages

--- resources/views/admin/roles-permissions.blade.php
+++ resources/views/admin/roles-permissions.blade.php
@@ -27,7 +27,7 @@
 
                 <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
                     <div class="table-responsive">
-                        <table class="table table-striped custom-table">
+                        <table class="table table-striped custom-table datatable">
                             <thead>
                                 <tr>
                                     <th>Role</th>
