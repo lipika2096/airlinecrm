f62d03ddd95ac3377e88a447cb549f2ab72f74b1
Lipika <dev.pentasoft@gmail.com>
2025-03-18 14:37:09 +0530

    solved leaves page error

--- resources/views/admin/leaves.blade.php
+++ resources/views/admin/leaves.blade.php
@@ -287,8 +287,8 @@
                                             <tr>
                                                 <td>
                                                     <h2 class="table-avatar">
-                                                        <a href="#">{{ $data->user->first_name }}
-                                                            {{ $data->user->last_name }} </a>
+                                                        <a href="#">{{ $data->user->first_name ??'N/A' }}
+                                                            {{ $data->user->last_name ?? ' ' }} </a>
                                                     </h2>
                                                 </td>
                                                 <td>{{$data->created_at}}</td>
